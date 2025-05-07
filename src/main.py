5974895604036e2db13868e8cac7c9fd07338193
juan.ezquerro <juan.ezquerro@devoteam.com>
2025-05-07 23:27:07 +0200

    refactor

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
-from ai_shell_assistant.main import main
+from ai_shell_assistant.assistant import main
+
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
     sys.exit(main())
